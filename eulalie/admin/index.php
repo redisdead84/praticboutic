bacad8bae7b48980ae45525fe0def9ce451b6302
Frédéric Legrand <flegrand.info@gmail.com>
2020-10-28 21:32:53 +0100

    Versionning css + js for cache

--- eulalie/admin/index.php
+++ eulalie/admin/index.php
@@ -4,7 +4,7 @@
     <title>Bienvenue sur le menu de la poisonnerie d'Eulalie</title>
     <meta name="viewport" content="initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
-    <link rel="stylesheet" href="css/back.css">
+    <link rel="stylesheet" href="css/back.css?v=1.0">
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="Expires" content="0" />
