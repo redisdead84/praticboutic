5d6d6f86db9598846608f301ada40c0b6c8b7cf0
Legrand Frédéric <flegrand.info@gmail.com>
2021-02-12 14:02:15 +0100

    fixcgv
    there were 2 session_start();

--- common/CGV.php
+++ common/CGV.php
@@ -15,8 +15,6 @@
 
   $method = isset($_GET ['method']) ? $_GET ['method'] : '0';
   $table = isset($_GET ['table']) ? $_GET ['table'] : '0';
-
-  session_start();
   
   if (strcmp($_SESSION[$customer .'_mail'],'oui') == 0)
   {
