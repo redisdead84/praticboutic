d1b421f3f1b93067b8c3ed399641c65658ec5393
Legrand Frédéric <flegrand.info@gmail.com>
2021-07-04 21:21:15 +0200

    bugfix_06
    Added directory boutic path

--- index.php
+++ index.php
@@ -9,8 +9,13 @@ Route::add('/',function(){
     header('LOCATION: https://www.pratic-boutic.fr');
 });
 
-// Accept only numbers as parameter. Other characters will result in a 404 error
+// boutic file path 
 Route::add('/([a-z0-9]+)',function($customer){
+    header('LOCATION: ../common/carte.php?method=' . 3 . '&customer=' . $customer);
+});
+
+// boutic directory path 
+Route::add('/([a-z0-9]+)/',function($customer){
     header('LOCATION: ../common/carte.php?method=' . 3 . '&customer=' . $customer);
 });
 
