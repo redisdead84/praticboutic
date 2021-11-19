66851e5b85f8f80b80a3cf34d59057f9f631791e
Legrand Frédéric <flegrand.info@gmail.com>
2021-11-19 12:11:55 +0100

    evo_deco_maimenu_001

--- common/customerarea/index.php
+++ common/customerarea/index.php
@@ -8,7 +8,7 @@
   <head>
     <meta name="viewport" content="initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Public+Sans' rel='stylesheet'>
-    <link rel="stylesheet" href="css/back.css?v=1.12">
+    <link rel="stylesheet" href="css/back.css?v=1.179">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -19,38 +19,47 @@
     <meta http-equiv="Expires" content="0" />
   </head>
   <body>
-    <div class="modal" tabindex="-1" role="dialog" data-backdrop="static">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <form method="post" action="valid.php">
-            <div class="modal-header">
-              <img id='logopbid' src='img/LOGO_PRATIC_BOUTIC.png' />
-              <br>
-               <h6 class="modal-title">CONNEXION ARRIERE BOUTIC</h6>
-             </div>
-            <div class="modal-body">
-              <div class="form-group">
-                <label>Courriel</label>
-                <input class="form-control" type="string" id="emailid" name="email">
+    <div id="screen">
+      <img id='bandeauh' src='img/bandeau_haut.png' onclick="window.location='https://pratic-boutic.fr'"/>
+      <div class="bigform-content">
+      <div class="modal modal-mainmenu" tabindex="-1" role="dialog" data-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-mainmenu" role="document">
+          <div class="modal-content modal-content-mainmenu">
+            <form method="post" action="valid.php">
+              <div class="modal-header modal-header-mainmenu">
+                <img id='logopbid' src='img/LOGO_PRATIC_BOUTIC.png' />
+                <h6 class="modal-title">CONNEXION ARRIERE BOUTIC</h6>
               </div>
-              <div class="form-group">
-                 <label>Mot de passe</label>
-                 <input class="form-control" type="password" id="passid" name="pass">
-               </div>
-            </div>
-            <div class="modal-footer">
-              <input class="btn btn-primary btn-block" type="submit" value="Valider"><br>
-              <a class="mr-auto mdfaddlink" href="./password.php">Mot de passe oublié ?</a><br>
-              <a class="mr-auto mdfaddlink" href="./reg.php">Je crée ma boutic</a>
-            </div>
-          </form>
+              <div class="modal-body modal-body-mainmenu">
+                <div class="form-group">
+                  <input class="form-control" placeholder="Courriel" type="string" id="emailid" name="email">
+                </div>
+                <div class="form-group">
+                   <input class="form-control" placeholder="Mot de passe" type="password" id="passid" name="pass">
+                 </div>
+              </div>
+              <div class="modal-footer">
+                <input class="btn btn-primary btn-block btn-valider" type="submit" value="VALIDER">
+                <a class="mr-auto mdfaddlink forgotpwd" href="./password.php">Mot de passe oublié ?</a>
+                <input class="btn btn-secondary btn-block btn-creationboutic" type="button" onclick="window.location='./reg.php'" value="JE CR&Eacute;E MA BOUTIC" />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
+      </div>
+      <img id='illus1' src='img/illustration_1.png' />
+      <img id='bandeaub' src='img/bandeau_bas.png' onclick="window.location='https://pratic-boutic.fr'"/>
     </div>
   </body>
   <script type="text/javascript" >
     $('.modal').modal({keyboard: false});
     $('.modal').modal('show');
-    //$('.modal').addClass('mdpb');
+    //appending modal background inside the bigform-content
+    $('.modal-backdrop').appendTo('.bigform-content');
+    //removing body classes to able click events
+    $('body').removeClass();
+    $('body').removeAttr('class');
+    $('body').removeAttr('style');
   </script>
 </html>
