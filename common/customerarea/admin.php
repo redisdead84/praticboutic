af5fb1633f7645754bdbb3c719da3744d76acbb5
Legrand Frédéric <flegrand.info@gmail.com>
2022-01-11 15:30:47 +0100

    fix_customerarea_049
    Fix bug https protocole

--- common/customerarea/admin.php
+++ common/customerarea/admin.php
@@ -82,14 +82,26 @@
       var login = "<?php echo $_SESSION['bo_email']; ?>";
       var init = "<?php echo $_SESSION['bo_init']; ?>";
       var initdone = "<?php $_SESSION['bo_init'] = 'non'; ?>";
-      var proto = "<?php echo $_SERVER['SERVER_PROTOCOL']; ?>";
-      proto = proto.toLowerCase();
+      //var proto = "<?php echo $_SERVER['SERVER_PROTOCOL']; ?>";
+      
+      //"<?php echo isset($_SERVER['HTTPS']); ?>";
+      var protocole = "<?php if (isset($_SERVER['HTTPS']))
+      {
+        echo 'https://';
+      }
+      else
+      {
+        echo 'http://';
+      }
+      ?>";
+      
+      /*proto = proto.toLowerCase();
       var protocole;
 
       if (proto.indexOf('https') != -1)
         protocole = 'https://';
       else
-        protocole = 'http://';
+        protocole = 'http://';*/
 
       var server = "<?php echo $_SERVER['SERVER_NAME']; ?>";
       var pathimg = '../../upload/';
