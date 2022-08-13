005db06fd97b312986752016da6eb220f4295c35
Legrand Frédéric <flegrand.info@gmail.com>
2022-08-13 10:11:28 +0200

    bug_fix11_primitives

--- common/primitives/close.php
+++ common/primitives/close.php
@@ -1,3 +1,14 @@
-<script>
-window.close();
-</script>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Fin de la procédure</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+  </head>
+  <body ondragstart="return false;" ondrop="return false;">
+	<h1>Fin de la procédure</h1>
+  </body>
+</html>
