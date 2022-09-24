f6aa13646f70cbeb2ebb27b59cbcd1119c987831
Legrand Frédéric <flegrand.info@gmail.com>
2022-09-24 16:55:21 +0200

    fix_security_part4

--- common/primitives/return.php
+++ common/primitives/return.php
@@ -11,51 +11,53 @@
   include "../config/common_cfg.php";
   include "../param.php";
 
-  if (empty($_SESSION['bo_auth']) == TRUE)
+  try
   {
-    exit();
-  }
-
-  if (strcmp($_SESSION['bo_auth'],'oui') != 0)
-  {
-    exit();
-  }
+    if (empty($_SESSION['bo_auth']) == TRUE)
+    {
+      throw new Error("Non authentifié");
+    }
   
-  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-  $dotenv->load();
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $bdd);
-  // Check connection
-  if ($conn->connect_error)
-  {
-    throw new Error("Connection failed: " . $conn->connect_error);
-  }
-
-  $json_str = file_get_contents('php://input');
-  $input = json_decode($json_str);
-  $output ="";
+    if (strcmp($_SESSION['bo_auth'],'oui') != 0)
+    {
+      throw new Error("Non authentifié");
+    }
+    
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $bdd);
+    // Check connection
+    if ($conn->connect_error)
+    {
+      throw new Error("Connection failed: " . $conn->connect_error);
+    }
+    
+    $sca = GetValeurParam("STRIPE_ACCOUNT_ID", $conn, $_SESSION['bo_id']);
+    
+    // For sample support and debugging. Not required for production:
+    \Stripe\Stripe::setAppInfo(
+      "pratic-boutic/registration  ",
+      "0.0.2",
+      "https://praticboutic.fr"
+    );
   
-  $sca = GetValeurParam("STRIPE_ACCOUNT_ID", $conn, $_SESSION['bo_id']);
-  
-  // For sample support and debugging. Not required for production:
-  \Stripe\Stripe::setAppInfo(
-    "pratic-boutic/registration  ",
-    "0.0.2",
-    "https://praticboutic.fr"
-  );
-
-  $stripe = new \Stripe\StripeClient([
-    'api_key' => $_ENV['STRIPE_SECRET_KEY'],
-    'stripe_version' => '2020-08-27',
-  ]);
-  
-  if (strcmp($sca,'') != 0)
-  {
-    if ($stripe->accounts->retrieve($sca, [])->details_submitted == true)
+    $stripe = new \Stripe\StripeClient([
+      'api_key' => $_ENV['STRIPE_SECRET_KEY'],
+      'stripe_version' => '2020-08-27',
+    ]);
+    
+    if (strcmp($sca,'') != 0)
     {
       $loginlink = $stripe->accounts->createLoginLink($sca);
-      error_log($loginlink->url);
       header('Location: ' .  $loginlink->url);
     }
-  }
+    else 
+    {
+      throw new Error('Il n\'y a pas de compte Stripe défini pour cette boutic');
+    }
+   catch (Error $e)
+   {
+     echo $e->getMessage();
+   }
 ?>
