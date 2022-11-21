ea3056857a12a8eda9fb16fc9a66e4dad8e57e9f
Legrand Frédéric <flegrand.info@gmail.com>
2022-11-21 16:17:53 +0100

    test_responsive_part6

--- common/customerarea/index.php
+++ common/customerarea/index.php
@@ -24,8 +24,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset.css?v=1.000">
-    <link rel="stylesheet" href="css/back.css?v=1.720">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -45,15 +44,8 @@
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="Expires" content="0" />
     <meta name="theme-color" content="#e2007a">
-    <style type="text/css">
-      @media only screen and (max-width > 1280px)
-      {
-        #workspace
-        {
-          background-color: pink !important;
-        }
-      }
-    </style>
+    <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset.css?v=1.000">
+    <link rel="stylesheet" href="css/back.css?v=1.721">
   </head>
   <body ondragstart="return false;" ondrop="return false;">
     <div id="screen">
