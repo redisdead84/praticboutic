9f153b1106ddf0e67c46069fabfb929cb33bca40
Legrand Frédéric <flegrand.info@gmail.com>
2022-11-21 18:35:27 +0100

    evo_validation_part1

--- common/customerarea/index.php
+++ common/customerarea/index.php
@@ -15,8 +15,9 @@
   $dotenv->load();
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="fr">
   <head>
+    <title>Accueil</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -30,7 +31,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-    <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="<?php echo $_ENV['CRISP_WEBSITE_ID']; ?>";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+    <script>window.$crisp=[];window.CRISP_WEBSITE_ID="<?php echo $_ENV['CRISP_WEBSITE_ID']; ?>";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
     <script src="https://accounts.google.com/gsi/client" async defer></script>
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $_ENV['GOOGLE_ANALYTICS_ID']; ?>"></script>
@@ -41,15 +42,15 @@
     
       gtag('config', '<?php echo $_ENV['GOOGLE_ANALYTICS_ID']; ?>');
     </script>
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-    <meta http-equiv="Pragma" content="no-cache" />
-    <meta http-equiv="Expires" content="0" />
+    <!--<meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate" />-->
+    <!--<meta http-equiv="Pragma" content="no-cache" />-->
+    <!--<meta http-equiv="Expires" content="0" />-->
     <meta name="theme-color" content="#e2007a">
     <link rel="stylesheet" href="css/safari.css?v=1.001">
   </head>
   <body ondragstart="return false;" ondrop="return false;">
     <div id="screen">
-      <img id='bandeauh' src='img/bandeau_haut.png' onclick="quittermenu()" class="epure"/>
+      <img id='bandeauh' src='img/bandeau_haut.png' onclick="quittermenu()" class="epure" alt="">
       <div id="workspace" class="spacemodal">
         <div id="loadid" class="spinner-border" role="status" style="display: none;">
           <span class="sr-only">Loading...</span>
@@ -58,12 +59,12 @@
           <div class="modal-content-mainmenu modal-content-cb">
             <form method="post" action="valid.php">
               <div class="modal-header-mainmenu modal-header-cb">
-                <img id='logopbid' src='img/LOGO_PRATIC_BOUTIC.png' />
+                <img id='logopbid' src='img/LOGO_PRATIC_BOUTIC.png' alt="">
                 <h6 class="modal-title-cb">CONNEXION ARRI&Egrave;RE BOUTIC</h6>
               </div>
               <div class="modal-body-mainmenu modal-body-cb">
                 <div class="form-group enlarged">
-                  <input class="form-control" placeholder="Courriel" type="string" id="emailid" name="email">
+                  <input class="form-control" placeholder="Courriel" type="text" id="emailid" name="email">
                 </div>
                 <div class="form-group enlarged">
                    <input class="form-control" placeholder="Mot de passe" type="password" id="passid" name="pass">
@@ -71,7 +72,7 @@
               </div>
               <div class="modal-footer-mainmenu">
                 <input class="btn btn-primary btn-block btn-valider" type="submit" value="VALIDER">
-                <input class="btn btn-secondary btn-block btn-creationboutic" type="button" onclick="window.location='./reg.php'" value="JE CR&Eacute;E MA BOUTIC" />
+                <input class="btn btn-secondary btn-block btn-creationboutic" type="button" onclick="window.location='./reg.php'" value="JE CR&Eacute;E MA BOUTIC">
                 <div id="g_id_onload" data-client_id="<?php echo $_ENV['GOOGLE_CLIENTID']; ?>" data-callback="handleCredentialResponse" data-auto_prompt="false"></div>
                 <div class="g_id_signin" data-type="standard" data-size="large" data-theme="outline" data-text="sign_in_with" data-shape="rectangular" data-logo_alignment="left"></div>
               </div>
@@ -79,12 +80,12 @@
           </div>
           <a class="mr-auto mdfaddlink forgotpwd" href="./password.php">Mot de passe oublié ?</a>
         </div>
-        <img id='illus1' src='img/illustration_1.png' class='elemcb epure' />
+        <img id='illus1' src='img/illustration_1.png' class='elemcb epure' alt="">
       </div>
-      <img id='bandeaub' src='img/bandeau_bas.png' onclick="quittermenu()" class="epure"/>
+      <img id='bandeaub' src='img/bandeau_bas.png' onclick="quittermenu()" class="epure" alt="">
     </div>
   </body>
-  <script type="text/javascript" >
+  <script>
     function quittermenu() 
     {
       if (confirm("Voulez-vous quitter ?") == true)
@@ -96,7 +97,7 @@
       }
     }
   </script>
-  <script type="text/javascript" >
+  <script>
     function decodeJwtResponse(token) 
     {
       var base64Url = token.split('.')[1];
@@ -140,7 +141,7 @@
         })
     }
   </script>
-  <script type="text/javascript" >
+  <script>
     sessionStorage.clear();
   </script>
 </html>
