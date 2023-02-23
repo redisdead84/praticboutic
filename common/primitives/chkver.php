d13430e0f77c3e0d8c067964d5ef890b6af2c0f9
Legrand Frédéric <flegrand.info@gmail.com>
2023-02-23 07:39:27 +0100

    evo_chkver_part1

---
+++ common/primitives/chkver.php
@@ -0,0 +1,50 @@
+<?php
+
+  header('Access-Control-Allow-Origin: *');
+  header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
+  header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  header ("Access-Control-Allow-Headers: Content-Type, Authorization, Accept, Accept-Language, X-Authorization");
+  header('Access-Control-Max-Age: 86400');
+
+  require '../../vendor/autoload.php';
+  include "../config/common_cfg.php";
+  include "../param.php";
+  
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+  $dotenv->load();
+
+  try
+  {
+    $postdata = file_get_contents("php://input");
+    if (isset($postdata))
+    {
+      $request = json_decode($postdata);
+    }
+    
+    if (isset($input->sessionid))
+      session_id($input->sessionid);
+    session_start();
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $bdd);
+    // Check connection
+    if ($conn->connect_error) {
+      throw new Error("La connexion a échoué: " . $conn->connect_error);
+    }
+    
+    $authofile = fopen('../mobileapp/authorisation.json', 'r');
+    $authojson = fread($authofile, filesize('../mobileapp/authorisation.json'));
+    fclose($authofile);
+
+    $output = $authojson;
+
+    $result->close();
+    $conn->close();
+    echo json_encode($output);
+  } 
+  catch (Error $e) 
+  {
+    http_response_code(500);
+    echo json_encode(['error' => $e->getMessage()]);
+  }
+?>
