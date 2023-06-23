426c5c88abda8859f57e977ee34bda4627c9f29b
Legrand Frédéric <flegrand.info@gmail.com>
2023-06-23 23:52:52 +0200

    evo_cors_part002

--- common/fraislivr.php
+++ common/fraislivr.php
@@ -1,31 +1,15 @@
 <?php
 
-session_start();
-
-if (empty($_SESSION['customer']) != 0)
-{
-  header('LOCATION: error.php?code=nocustomer');
-  exit();
-}
-
-$customer = $_SESSION['customer'];
-$method = $_SESSION['method'];
-$table = $_SESSION['table'];
-
-if (empty($_SESSION[$customer . '_mail']) == TRUE)
-{
-  header('LOCATION: error.php?code=noemail');
-  exit();
-}
-
-if (strcmp($_SESSION[$customer . '_mail'],'oui') == 0)
-{
-  header('LOCATION: error.php?code=alreadysent');
-  exit();
-}
-
 require '../vendor/autoload.php';
+include "config/common_cfg.php";
+include "param.php";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $bdd);
+// Check connection
+if ($conn->connect_error) 
+  die("Connection failed: " . $conn->connect_error); 
+  
 header('Access-Control-Allow-Origin: *');
 header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
 header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
@@ -33,25 +17,44 @@ header ("Access-Control-Allow-Headers: Content-Type, Authorization, Accept, Acce
 header('Access-Control-Max-Age: 86400');
 header('Content-Type: application/json');
 
+
+
 try {
 
   $json_str = file_get_contents('php://input');
   $obj = json_decode($json_str);
+  if (isset($obj->sessionid))
+    session_id($obj->sessionid);
+
+  session_start();
+
+  if (empty($_SESSION['customer']) != 0)
+  {
+    header('LOCATION: error.php?code=nocustomer');
+    exit();
+  }
+
+  $customer = $_SESSION['customer'];
+  $method = $_SESSION['method'];
+  $table = $_SESSION['table'];
+
+  if (empty($_SESSION[$customer . '_mail']) == TRUE)
+  {
+    header('LOCATION: error.php?code=noemail');
+    exit();
+  }
+
+  if (strcmp($_SESSION[$customer . '_mail'],'oui') == 0)
+  {
+    header('LOCATION: error.php?code=alreadysent');
+    exit();
+  }
   
   $customer = htmlspecialchars($obj->customer);
-  
-  include "config/common_cfg.php";
-  include "param.php";
 
   $val = $obj->sstotal;
   
   $surcout = 0;
-  
-		// Create connection
-	$conn = new mysqli($servername, $username, $password, $bdd);
-	// Check connection
-	if ($conn->connect_error) 
-	  die("Connection failed: " . $conn->connect_error); 
 	
 	$reqci = $conn->prepare('SELECT customid FROM customer WHERE customer = ?');
 	$reqci->bind_param("s", $customer);
