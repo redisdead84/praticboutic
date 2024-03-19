de8ab99865ea4c77358cef99d83753be961fdf7f
Legrand Frédéric <flegrand.info@gmail.com>
2024-03-19 20:33:10 +0100

    cleanup_oldfrontend_part001

--- common/customerarea/exit.php
+++
@@ -1,5 +0,0 @@
-<?php
-  session_start();
- 	header("LOCATION: https://pratic-boutic.fr");
- 	exit();
-?>
--- common/customerarea/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-  session_start();
-  $_SESSION["active"] = 0;
-  header("LOCATION: index.php");
-  exit();
-?>
